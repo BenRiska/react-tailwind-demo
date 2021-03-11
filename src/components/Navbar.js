5605872a16c4b3937ff2b0abed7b87a47ef3901b
bentaylor <ben10taylor10@hotmail.com>
2021-03-11 18:17:53 +0000

    redesigning the site with embers landing page using tailwind

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import emberLogo from "../images/ember.svg"
 
 const Navbar = () => {
     return (
         <nav
-      className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
+      className='container mx-auto flex justify-between items-center h-20 bg-white text-black relative'
       role='navigation'
     >
-      <Link to='/' className='pl-8'>
-        EGG
-      </Link>
-      <div className='px-4 cursor-pointer md:hidden'>
-        <svg
-          className='w-8 h-8'
-          fill='none'
-          stroke='currentColor'
-          viewBox='0 0 24 24'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            d='M4 6h16M4 12h16M4 18h16'
-          />
-        </svg>
+      <div className="flex justify-between items-center">
+        <Link to='/' className='px-4 cursor-pointer'>
+          <img className="h-7" src="https://images.prismic.io/ember-bank/72b3bd16-d681-4ce3-bfab-674761131a4b_Group+8.png?auto=compress,format&rect=0,0,2618,691&w=197&h=52" alt="ember logo"/>
+        </Link>
+        <div className='pr-8 lg:block ml-4 space-x-6 hidden'>
+          <Link to='/' className='p-4 relative font-bold group'>
+            Who we're for
+            <div className="absolute space-y-2 hidden group-hover:block shadow-lg w-52 p-4">
+              <p className="font-normal">Freelancers</p>
+              <p className="font-normal">Contracters</p>
+              <p className="font-normal">Startups</p>
+              <p className="font-normal">Small businesses</p>
+            </div>
+          </Link>
+          <Link to='/menu' className='p-4 font-bold'>
+            Pricing
+          </Link>
+          <Link to='/about' className='p-4 font-bold'>
+            Partner
+          </Link>
+          <Link to='/contact' className='p-4 font-bold'>
+            Blog
+          </Link>
+        </div>
       </div>
-      <div className='pr-8 md:block  hidden'>
-        <Link to='/' className='p-4'>
-          Home
-        </Link>
-        <Link to='/menu' className='p-4'>
-          Menu
-        </Link>
-        <Link to='/about' className='p-4'>
-          About
-        </Link>
-        <Link to='/contact' className='p-4'>
-          Contact
-        </Link>
+      <div className="space-x-4">
+        <button className="py-2 px-8 bg-gray-100 font-bold rounded">Login</button>
+        <button className="py-2 px-8 font-bold bg-red-400 text-white rounded">Try 1 month free</button>
       </div>
     </nav>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      "md": "760px",
+      "lg": "1200px"
+    },
     extend: {},
   },
   variants: {
+    display: ['responsive', 'group-hover', 'group-focus'],
     extend: {},
   },
   plugins: [],
